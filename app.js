0235bc57388e949ca8553fcb6f45dbbcec5646c7
Henrique-mazzoleni <henrique.mazzoleni@gmail.com>
2022-12-08 21:38:03 +0100

    added views, loaded endpoints, linked stylesheets

--- app.js
+++ app.js
@@ -2,8 +2,18 @@ const express = require("express")
 
 const app = express()
 
+app.use(express.static('public'))
+
+app.get("/", (req,res)=>{
+    res.sendFile(__dirname +"/views/home.html")
+})
+
 app.get("/about", (req,res)=>{
     res.sendFile(__dirname +"/views/about.html")
 })
 
+app.get("/works", (req,res)=>{
+    res.sendFile(__dirname +"/views/works.html")
+})
 
+app.listen(4000)
