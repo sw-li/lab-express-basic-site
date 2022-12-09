4be27ebba08034d11a077b67498da067d32b248b
Henrique-mazzoleni <henrique.mazzoleni@gmail.com>
2022-12-09 11:42:35 +0100

    lab solved

--- app.js
+++ app.js
@@ -4,16 +4,20 @@ const app = express()
 
 app.use(express.static('public'))
 
-app.get("/", (req,res)=>{
+app.get("/", (_,res)=>{
     res.sendFile(__dirname +"/views/home.html")
 })
 
-app.get("/about", (req,res)=>{
+app.get("/about", (_,res)=>{
     res.sendFile(__dirname +"/views/about.html")
 })
 
-app.get("/works", (req,res)=>{
+app.get("/works", (_,res)=>{
     res.sendFile(__dirname +"/views/works.html")
+})
+
+app.get("/galery", (_, res)=>{
+    res.sendFile(__dirname + "/views/galery.html")
 })
 
 app.listen(3000)
